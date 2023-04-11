d0c0c8c44107f1cd4931b872b6eb6bea6732b942
tomict23 <tct23@DESKTOP-EAGGD5K>
2023-04-11 15:36:28 -0500

    Footer finished

--- nike-clone/src/components/Footer.js
+++ nike-clone/src/components/Footer.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import '../styles/Footer.css';
 import locatorSVG from '../styles/svgs/locator.svg';
 import twitterLogo from '../styles/svgs/twitter.svg';
@@ -8,9 +10,22 @@ import instagramLogo from '../styles/svgs/instagram.svg';
 const linksLeft = ['GIFT CARDS', 'PROMOTIONS', 'FIND A STORE', 'SIGN UP FOR EMAIL', 'BECOME A MEMBER', 'NIKE JOURNAL', 'SEND US FEEDBACK'];
 const linksMiddle = ['Order Status', 'Shipping and Delivery', 'Returns', 'Order Cancellation', 'Payment Options', 'Gift Card Balance', 'Contact Us'];
 const linksRight = ['News', 'Careers', 'Investors', 'Purpose', 'Sustainability'];
+const linksBottomRightTop = ['Terms of Sale', 'Terms of Use', 'Nike Privacy Policy', 'Your Privacy Choices'];
 
-const linksBottomRightTop = ['Guides', 'Terms of Sale', 'Terms of Use', 'Nike Privacy Policy', 'Your Privacy Choices'];
+const guideLinks1 = ['Nike Adapt', 'Nike Air Max', 'Nike Flyleather', 'Nike React', 'Space Hippie'];
+const guideLinks2 = ['Nike Air', 'Nike FlyEase', 'Nike Free', 'Nike Vaporfly'];
+const guideLinks3 = ['Nike Air Force 1', 'Nike Flyknit', 'Nike Joyride', 'Nike ZoomX'];
+
 const Footer = () => {
+  const [display, setDisplay] = useState('none');
+  const guideMenuShow = () => {
+    setDisplay('flex');
+  };
+
+  const guideMenuHide = () => {
+    setDisplay('none');
+  };
+
   const hoverOn = (event) => {
     event.target.style.transition = 'opacity 0.1s ease-in-out';
     event.target.style.opacity = 1;
@@ -93,7 +108,58 @@ const Footer = () => {
             <p id='copyright-disc'>© 2023 Nike, Inc. All Rights Reserved</p>
           </div>
           <div id='footer-bottom-right'>
+            <div
+              className='guide-menu'
+              style={{ display: display }}
+              onMouseOut={guideMenuHide}
+            >
+              <div
+                className='guide-list'
+                onMouseOver={guideMenuShow}
+              >
+                {guideLinks1.map((link) => (
+                  <p
+                    className='link-gray'
+                    onMouseOver={guideMenuShow}
+                  >
+                    {link}
+                  </p>
+                ))}
+              </div>
+              <div
+                className='guide-list'
+                onMouseOver={guideMenuShow}
+              >
+                {guideLinks2.map((link) => (
+                  <p
+                    className='link-gray'
+                    onMouseOver={guideMenuShow}
+                  >
+                    {link}
+                  </p>
+                ))}
+              </div>
+              <div
+                className='guide-list'
+                onMouseOver={guideMenuShow}
+              >
+                {guideLinks3.map((link) => (
+                  <p
+                    className='link-gray'
+                    onMouseOver={guideMenuShow}
+                  >
+                    {link}
+                  </p>
+                ))}
+              </div>
+            </div>
             <div id='f-bottom-right-top'>
+              <p
+                onMouseOver={guideMenuShow}
+                className='link-gray'
+              >
+                Guides
+              </p>
               {linksBottomRightTop.map((link) => (
                 <p className='link-gray'>{link}</p>
               ))}
