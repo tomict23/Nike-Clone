908322e5d518043ca4b3261e1adfb9b9c15b435d
tomict23 <tct23@DESKTOP-EAGGD5K>
2023-04-12 17:09:23 -0500

    Suggetion component layout done

--- nike-clone/src/components/MainBody.js
+++ nike-clone/src/components/MainBody.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import "../styles/App.css";
-import "../styles/MainBody.css";
-import HeroContainer from "./HeroContainer";
-import RightBodyContainer from "./RightBodyContainer";
+import '../styles/App.css';
+import '../styles/MainBody.css';
+import HeroContainer from './HeroContainer';
+import RightBodyContainer from './RightBodyContainer';
 
 import SuggestedContent from './SuggestedContent';
 
@@ -10,18 +10,20 @@ const MainBody = () => {
   const [id, setId] = useState(1);
 
   return (
-    <div className="main-body-container">
-      <div className="main-body">
-        <div className="main-body-left">
-          <HeroContainer id={id}/>
-        </div>
+    <>
+      <div className='main-body-container'>
+        <div className='main-body'>
+          <div className='main-body-left'>
+            <HeroContainer id={id} />
+          </div>
 
-        <div className="main-body-right">
-          <RightBodyContainer setId={setId}/>
+          <div className='main-body-right'>
+            <RightBodyContainer setId={setId} />
+          </div>
         </div>
-          <SuggestedContent/>
       </div>
-    </div>
+      <SuggestedContent />
+    </>
   );
 };
 
