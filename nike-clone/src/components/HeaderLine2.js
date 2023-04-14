b64fa68d41e315864ae4adea5a181673c6e5750c
Taylor Lovett <tclovett7@gmail.com>
2023-04-14 17:32:35 -0500

    when you click a button in ShoeButtonGroup, it will now have a border to indicate it is selected

--- nike-clone/src/components/HeaderLine2.js
+++ nike-clone/src/components/HeaderLine2.js
@@ -111,6 +111,27 @@ const HeaderLine2 = () => {
       <svg   aria-hidden="true" className="cart" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M8.25 8.25V6a2.25 2.25 0 012.25-2.25h3a2.25 2.25 0 110 4.5H3.75v8.25a3.75 3.75 0 003.75 3.75h9a3.75 3.75 0 003.75-3.75V8.25H17.5"></path></svg>
     </div>)
   }, [searchStatus === null])
+  function onClick() {
+    setSearchStatus((
+      <div className='searchMorph'>
+        <div className="nikeLogoContainer">
+          <svg className="nikeLogo" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
+              <path fill="currentColor" fill-rule="evenodd" d="M21 8.719L7.836 14.303C6.74 14.768 5.818 15 5.075 15c-.836 0-1.445-.295-1.819-.884-.485-.76-.273-1.982.559-3.272.494-.754 1.122-1.446 1.734-2.108-.144.234-1.415 2.349-.025 3.345.275.2.666.298 1.147.298.386 0 .829-.063 1.316-.19L21 8.719z" clip-rule="evenodd">
+                </path>
+            </svg>
+        </div>
+        <div className='searchMenu morph'>
+          <svg  class="searchIcon" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
+          <path stroke="currentColor" stroke-width="1.5" d="M13.962 16.296a6.716 6.716 0 01-3.462.954 6.728 6.728 0 01-4.773-1.977A6.728 6.728 0 013.75 10.5c0-1.864.755-3.551 1.977-4.773A6.728 6.728 0 0110.5 3.75c1.864 0 3.551.755 4.773 1.977A6.728 6.728 0 0117.25 10.5a6.726 6.726 0 01-.921 3.407c-.517.882-.434 1.988.289 2.711l3.853 3.853">
+          </path>
+          </svg>
+          <input type="text" class="searchBar" name="search" autocomplete="off" data-var="vsInput" tabindex="0" placeholder="Search" aria-label="Search Products" role="combobox" aria-controls="VisualSearchSuggestionsList" aria-owns="VisualSearchSuggestionsList" aria-expanded="false">
+          </input>
+        </div>
+      </div>
+    ))
+  }
+
   return (
     <div className='Headerline2Container'>
       <div className='HeaderLine2'>
