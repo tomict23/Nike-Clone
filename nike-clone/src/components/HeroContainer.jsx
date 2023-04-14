214d2d06cd2eac0c110e7c29ac61d4bfb1ecfbe3
tomict23 <tct23@DESKTOP-EAGGD5K>
2023-04-14 12:08:57 -0500

    CSS fixes on suggested content, thumbnails

--- nike-clone/src/components/HeroContainer.jsx
+++ nike-clone/src/components/HeroContainer.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import HeroThumbnails from "./HeroThumbnails";
-import Carousel from "./Carousel";
+import React, { useEffect, useState } from 'react';
+import HeroThumbnails from './HeroThumbnails';
+import Carousel from './Carousel';
 
 const HeroContainer = (props) => {
   // State management for carousel buttons to cycle through array of images
@@ -9,7 +9,7 @@ const HeroContainer = (props) => {
 
   // Default images to load on initial render
   useEffect(() => {
-    fetch("http://localhost:8000/api/shoes/1")
+    fetch('http://localhost:8000/api/shoes/1')
       .then((res) => res.json())
       .then((data) => {
         props.setFocusedImage(data[0].image);
@@ -17,9 +17,9 @@ const HeroContainer = (props) => {
         props.setSizeArray(data[0].size_array);
         props.setPrice(data[0].price);
         props.setInterest('$31.25');
-        props.setCurrentShoe(data)
-    })
-      .catch((error) => console.error(error))
+        props.setCurrentShoe(data);
+      })
+      .catch((error) => console.error(error));
   }, []);
 
   // Carousel button controls
@@ -41,41 +41,43 @@ const HeroContainer = (props) => {
   // Sets focused image when mouse enters a thumbnail image
   const handleEnter = (image) => {
     props.setFocusedImage(image);
+    console.log(image);
   };
 
   useEffect(() => {
-    if(props.data){
+    if (props.data) {
       const amtReviews = props.data.length;
       let starArray = [];
       for (let i = 0; i < amtReviews; i++) {
-          starArray.push(props.data[i].stars)
+        starArray.push(props.data[i].stars);
       }
       const sumOfStars = starArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
       const avgOfStars = Math.ceil(sumOfStars / starArray.length);
-        if(avgOfStars >= 4){
-          setHighlyRated(
-            <div className= "HR">
-              <span className="HRstar">&#9733;</span>
-              <p className='HRText'>
-                Highly Rated
-              </p>
-            </div>
-          );
-        }
+      if (avgOfStars >= 4) {
+        setHighlyRated(
+          <div className='HR'>
+            <span className='HRstar'>&#9733;</span>
+            <p className='HRText'>Highly Rated</p>
+          </div>
+        );
+      }
     }
   }, [props.data]);
-  
-  
+
   // Rendering
   return (
-    <div className="hero-container">
+    <div className='hero-container'>
       <HeroThumbnails
         handleEnter={handleEnter}
         thumbnailImages={props.thumbnailImages}
       />
-      <div className="hero-image-container">
+      <div className='hero-image-container'>
         {highlyRated}
-        <img id="hero-img" src={props.focusImage} alt="" />
+        <img
+          id='hero-img'
+          src={props.focusImage}
+          alt=''
+        />
         <Carousel
           prevImage={prevImage}
           nextImage={nextImage}
